90fa7ebd174eafd1ebc0817a1c7865cadcb8b1c3
e.levin <e.levin@solarlab.ru>
2024-12-05 21:06:28 +0300

    Добавил возможность интеграции API с JS

---
+++ src/main/java/com/pewde/profileservice/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.pewde.profileservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SecurityConfig implements WebMvcConfigurer {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(request -> request.anyRequest().permitAll())
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+        return http.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("*");
+    }
+
+}
