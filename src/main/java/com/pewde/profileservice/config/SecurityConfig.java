4d10989c22240da182b06c719d2b415e7cf7a737
e.levin <e.levin@solarlab.ru>
2024-12-09 11:56:24 +0300

    Добавил возможность отправлять все виды запросов

--- src/main/java/com/pewde/profileservice/config/SecurityConfig.java
+++ src/main/java/com/pewde/profileservice/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,7 +15,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(request -> request.anyRequest().permitAll())
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll);
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll).csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
